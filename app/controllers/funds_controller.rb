105cd20e4708fbba21e07408c3b3d28a2dc4e850
Delaine <delaine.lawrence35@gmail.com>
2015-09-23 20:38:31 +0000

    Making the Submit button create a new pledge

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -6,4 +6,15 @@ class FundsController < ApplicationController
   def new
     @fund = Fund.new
   end
+
+  def create
+    Fund.create(fund_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def fund_params
+    params.require(:fund).permit(:name, :address, :city, :zip, :phone, :email)
+  end
 end
