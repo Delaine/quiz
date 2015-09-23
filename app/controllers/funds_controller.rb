20fe009bfc37972d368308c710a083ea8798cedc
Delaine <delaine.lawrence35@gmail.com>
2015-09-23 16:24:58 +0000

    Added Bootstrap gem

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -2,4 +2,8 @@ class FundsController < ApplicationController
   def index
     
   end
+
+  def new
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
    root 'funds#index'
+
+   resources :funds
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
