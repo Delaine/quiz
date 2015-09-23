7cfcd4dc5a01bcae57ac81fd5e36a17e289149ea
Delaine <delaine.lawrence35@gmail.com>
2015-09-23 12:52:36 +0000

    Setting up the models and the database for funds

---
+++ db/migrate/20150923124756_create_funds.rb
@@ -0,0 +1,13 @@
+class CreateFunds < ActiveRecord::Migration
+  def change
+    create_table :funds do |t|
+
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
